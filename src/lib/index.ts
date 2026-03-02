1003d77b0a7dc7da65bb616e18d3d045c3d7aa02
miruky <h.miyoki@gmail.com>
2026-03-02 12:54:23 +0900

    ライブラリの公開APIを集約

---
+++ src/lib/index.ts
@@ -0,0 +1,19 @@
+export { AGE_LABELS, BIRTHS_5Y, INITIAL_1950, MALE_BIRTH_SHARE, MORTALITY } from './data';
+export {
+  END_YEAR,
+  START_YEAR,
+  STEP,
+  binOfCohort,
+  cohortStartOf,
+  fiveYearSurvival,
+  infantSurvival,
+  interpolateSnapshot,
+  mortalityRate,
+  projectSeries,
+} from './model';
+export { snapshotStats, formatPopulation, formatShare } from './stats';
+export type { SnapshotStats } from './stats';
+export { AXIS_MAX, LAYOUT, barRect, renderPyramid, rowTitle, rowY } from './render';
+export type { RenderOptions } from './render';
+export { BIN_COUNT } from './types';
+export type { PyramidSeries, PyramidSnapshot, Sex } from './types';
